373e5fc89ffc3a94e346c6261a458513f98ce5f3
Faizan Ali <syedfaizanali20@gmail.com>
2024-12-07 16:24:38 +0530

    Formatting changes

--- main.py
+++ main.py
@@ -3,40 +3,45 @@ import random
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 async def root():
-    return { 'example' : 'This is an example', 'data' : 1090 }
+    return {"example": "This is an example", "data": 1090}
 
-@app.get('/random')
+
+@app.get("/random")
 async def get_no():
-    rn: int = random.randint(1,100)
-    return {'number':rn,'limit':100}
+    rn: int = random.randint(1, 100)
+    return {"number": rn, "limit": 100}
 
-@app.get('/random/{limit}')
-async def set_limit(limit:int):
-    rn: int = random.randint(0,limit)
-    return {'number':rn,'limit':limit}
+
+@app.get("/random/{limit}")
+async def set_limit(limit: int):
+    rn: int = random.randint(0, limit)
+    return {"number": rn, "limit": limit}
+
 
 from openai import OpenAI
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 import os
 
-@app.get('/api')
+
+@app.get("/api")
 async def get_api(query: str):
 
-   load_dotenv()
-   client = OpenAI(api_key = os.getenv("OPENAI_API_KEY") ) 
-   # type: ignore
+    load_dotenv()
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    # type: ignore
 
-   completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": query}
-  ],
-     temperature=0.7
-)
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": query},
+        ],
+        temperature=0.7,
+    )
 
-   message = completion.choices[0].message.content
-   print(message)
-   return {"response": message}
+    message = completion.choices[0].message.content
+    print(message)
+    return {"response": message}
